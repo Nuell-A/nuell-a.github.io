fb893d8f4a526f6a1074377ebc3a7bfe645a68e7
Nuell <alcala.arredondo1@gmail.com>
2022-11-08 15:03:34 -0800

    First version of portfolio

---
+++ icons/Portfolio_files/main.js
@@ -0,0 +1,17 @@
+function avionics() {
+    var x = document.getElementById("avi");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
+
+function information() {
+    var x = document.getElementById("it");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
